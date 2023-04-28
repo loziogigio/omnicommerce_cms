b6ae23650472c268fa37134c8100b73643c5f286
jire87 <y.abia@crowdechain.com>
2023-04-28 09:37:19 +0200

    Starting point

---
+++ omnicommerce_cms/omnicommerce_cms/doctype/b2c_menu/b2c_menu.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Crowdechain s.r.o and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.utils.nestedset import NestedSet
+
+class B2CMenu(NestedSet):
+	pass
---
+++ omnicommerce_cms/omnicommerce_cms/doctype/promo_slider/test_promo_slider.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2023, Crowdechain s.r.o and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestPromoSlider(FrappeTestCase):
+	pass
