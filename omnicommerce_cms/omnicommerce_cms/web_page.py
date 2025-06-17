d4c33cc1a88e44f2c2bb5bef7db6a68bb6469efa
loziogigio <yiresse.abia@gmail.com>
2025-06-17 12:11:25 +0000

    updated problematic field for cache

--- omnicommerce_cms/omnicommerce_cms/web_page.py
+++ omnicommerce_cms/omnicommerce_cms/web_page.py
@@ -28,18 +28,25 @@ def get_web_page_detail(route=None):
         }
     
 
-@frappe.whitelist(allow_guest=True, methods=['GET'])
+
+import json
+from frappe.utils import cstr
+
+@frappe.whitelist(allow_guest=True, methods=["GET"])
 def update_hook_b2b_main_web_page(doc=None, method=None):
     cache_key = "b2b_web_page_detail:main"
-
-    # Recupera la pagina con route="main"
     web_page = frappe.get_all("Web Page", fields=["*"], filters={"route": "main"}, limit=1)
 
     if web_page:
-        data = web_page[0]
+        data = dict(web_page[0])
+
+        # Converti datetime in stringa se necessario
+        for key in ["creation", "modified", "start_date", "end_date"]:
+            if data.get(key):
+                data[key] = cstr(data[key])
+
         frappe.cache().set_value(cache_key, data)
         return {"data": data}
     else:
         frappe.cache().delete_value(cache_key)
         return {"error": "No Web Page found with route 'main'"}
-
